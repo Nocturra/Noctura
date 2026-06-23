e5f8210e4c6d063444f7cc7756a738ef646155f3
Noctura <chasehodgdon12@gmail.com>
2026-06-22 21:23:07 -0300

    Implement close GUI button in settings
    Added a button to close the GUI in the settings category.

--- scripts/SellLemons/script.lua
+++ scripts/SellLemons/script.lua
@@ -93,6 +93,7 @@ AutofarmCategory:AddToggle("Autofarm", false, function(bool)
     task.spawn(function()
         while getgenv().farming do
             if not getgenv().farmsettings.collect then wait(1) continue end
+
             for i, v in pairs(stands:GetChildren()) do
                 local Event = tycoon.Remotes.WakeIncomeStream
                 Event:InvokeServer(
@@ -104,6 +105,7 @@ AutofarmCategory:AddToggle("Autofarm", false, function(bool)
     end)
 
     while getgenv().farming do
+
         pcall(function()
             if not getgenv().farmsettings.purchase then return end
             for _, fold in pairs(PurchasesFold:GetChildren()) do
@@ -150,7 +152,6 @@ AutofarmCategory:AddToggle("Autofarm", false, function(bool)
                 end
             end
         end)
-
         pcall(function()
             if not getgenv().farmsettings.cashdrop then return end
             for i, v in pairs(workspace.CashDrops:GetChildren()) do
@@ -205,11 +206,14 @@ plr.Idled:Connect(function()
     game:GetService("VirtualUser"):Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
     game:GetService("VirtualUser"):Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
 end)
-
 SettingsCategory:AddToggle("Disable 3D Rendering", false, function(v)
     game:GetService("RunService"):Set3dRenderingEnabled(not v)
 end)
 
 SettingsCategory:AddToggle("Anti AFK", true, function(v)
     getgenv().antiafk = v
+end)
+
+SettingsCategory:AddButton("Close GUI", function()
+    Gui:Destroy()
 end)
